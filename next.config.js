c8dfc7ee65a0bc3560bc235155f1a9213cd0421d
Alcateiat Act <giovanni.a.marc@gmail.com>
2026-03-05 17:31:19 -0800

    fixes14

--- next.config.js
+++ next.config.js
@@ -18,7 +18,9 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@prisma/client', 'prisma'],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
