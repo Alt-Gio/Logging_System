110637fb2860a710b97da95d195794fbe990e31f
Alcateiat Act <giovanni.a.marc@gmail.com>
2026-03-23 15:10:31 +0800

    fix: remove conflicting catch-all sign-in and sign-up routes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function SignInCatchAll() {
-  redirect('/sign-in')
-}
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function SignUpCatchAll() {
-  redirect('/sign-in')
-}
